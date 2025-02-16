2e3b87b1144a27f50632ec5c2c2f0ec7986814e3
Rick Martin <rickm@comcast.net>
2025-02-16 15:23:16 -0500

    Added calc.py with task 5 complete

--- assignment1/calc.py
+++ assignment1/calc.py
@@ -11,5 +11,18 @@ if (num1 == 0):
     outDiv = "Can't divide by zero"
 else:
     outDiv = num2/num1
-    
+
 print (f"Division: {outDiv}")
+
+# Task 5
+try:
+    float1 = float (input ("Enter numerator:"))
+    float2 = float (input ("Enter denominator:"))
+    divResult = float1/float2
+    print (f"Division: {divResult}")
+except ZeroDivisionError:
+   print("You cannot divide a value with zero")
+except ValueError:
+    print("one of two inputs not a float")
+except:
+   print("Something else went wrong")

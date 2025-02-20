ebd062381158ef2aa53e2e101773f31904de1496
Rick Martin <rickm@comcast.net>
2025-02-19 19:27:45 -0500

    function perform_operations complete

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -1,2 +1,33 @@
 # Assignment 2
 # Put code here
+
+# Task 1
+def perform_operations (a, b):
+    print (f"Operations on {a} and {b}:")
+    print (f"  Multiplication: {a*b}")
+    print (f"  Addition: {a + b}")
+    print (f"  Subtraction: {a - b}")
+
+    # Division - check for 0
+    if not b:
+        print ("  Division: Can't divide by 0")
+    else:
+        print (f"  Division: {a/b}")
+    
+    # Floor division
+    if not b:
+        print ("  Floor division: Can't floor divide by 0")
+    else:
+        print (f"  Floor division: {a//b}")
+    
+    # Modulos
+    if not b:
+        print ("  Modulus 0 is not allowd")
+    else:
+        print (f"  Modulus: {a % b}")
+    
+    # Exponentiation
+    print ((f"  Exponentiation: {a**b}"))
+
+perform_operations (10,3)
+

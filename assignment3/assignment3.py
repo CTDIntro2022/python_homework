9b917c1ae3c7a0a3f688ab7eb40e2e5148bb0dba
Rick Martin <rickm@comcast.net>
2025-03-15 16:59:54 -0400

    All tasks completed

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -1,11 +1,12 @@
 # Author: Rick Martin
 import pandas as pd
+import numpy as np
 
 #Use a dictionary containing the following data:
 #  Name: ['Alice', 'Bob', 'Charlie']
 #  Age: [25, 30, 35]
 #  City: ['New York', 'Los Angeles', 'Chicago']
-dict = { "Name": ['Alice', 'Bob', 'Charlie'],
+dict = { "Name": ['Alice', 'Bob', 'charlie'],
         "Age": [25, 30, 35],
         "City": ['New York', 'Los Angeles', 'Chicago'] }
 task1_data_frame = pd.DataFrame(dict)
@@ -41,8 +42,57 @@ json_employees = pd.read_json(EMPJSON)
 # Combine the data from the JSON file into the DataFrame Loaded from the CSV file and save it in the variable more_employees.
 # Print the combined Dataframe and run the tests.
 more_employees = pd.concat([task2_employees, json_employees], ignore_index=True)
-print (more_employees)
+# print (more_employees)
 
 #========= TASK 3 ===========
+# Assign the first three rows of the more_employees DataFrame to the variable first_three
+first_three = more_employees.head(3)
 
+# Assign the last two rows of the more_employees DataFrame to the variable last_two
+last_two = more_employees.tail(2)
 
+# Assign the shape of the more_employees DataFrame to the variable employee_shape
+employee_shape = more_employees.shape
+
+# Print a concise summary of the DataFrame using the info() method to understand the data types and non-null counts.
+# print (more_employees.info())
+
+#========= TASK 4 ===========
+# Create a DataFrame from dirty_data.csv file and assign it to the variable dirty_data.
+DIRTYDATA = "dirty_data.csv"
+dirty_data = pd.read_csv(DIRTYDATA)
+print ("Dirty data: \n", dirty_data)
+
+# Remove any duplicate rows from the DataFrame
+clean_data = dirty_data.copy()
+clean_data = clean_data.drop_duplicates()
+print ("Dropped Dupes:\n", clean_data)
+
+# Convert Age to numeric and handle missing values
+clean_data["Age"] = pd.to_numeric(clean_data["Age"], errors="coerce")
+print ("Age to numberic:\n", clean_data)
+
+# Convert Salary to numeric and replace known placeholders (unknown, n/a) with NaN
+clean_data["Salary"] = pd.to_numeric(clean_data["Salary"], errors="coerce")
+print ("Salary to numberic:\n", clean_data)
+
+# Fill missing numeric values (use fillna).  Fill Age with the mean and Salary with the median
+ageMean = clean_data['Age'].mean()
+clean_data['Age'] = clean_data['Age'].fillna(ageMean) 
+
+salaryMedian = clean_data['Salary'].median()
+clean_data['Salary'] = clean_data['Salary'].fillna(salaryMedian)
+
+# print ("After replace na for salary:\n", clean_data)
+
+# Convert Hire Date to datetime
+clean_data ['Hire Date'] = pd.to_datetime(clean_data['Hire Date'], format='mixed')
+print ("After date time conversion:\n", clean_data)
+
+# Strip extra whitespace and standardize Name and Department as uppercase
+clean_data['Department'] = clean_data['Department'].str.upper()
+clean_data['Department'] = clean_data['Department'].str.strip()
+clean_data['Name'] = clean_data['Name'].str.upper()
+clean_data['Name'] = clean_data['Name'].str.strip()
+
+print ("After Department and name to Upper and white space:\n", clean_data)
